5e33b57e698acf3a1f49f902796ecb2d7b09dece
Saromazzotta <saromazzotta@proton.me>
2025-02-02 17:19:30 -0500

    added groups to manage multiple game objects

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    
 
     while True:
         for event in pygame.event.get():
@@ -22,8 +27,10 @@ def main():
                 return
             
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawings in drawable:
+            drawings.draw(screen)
+        # player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
