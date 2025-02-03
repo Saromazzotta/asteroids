a942e5c6d028dca8f324e76e85f61f5f4744b3b0
Saromazzotta <saromazzotta@proton.me>
2025-02-02 21:35:38 -0500

    refactored nested foor lop

--- main.py
+++ main.py
@@ -51,11 +51,10 @@ def main():
                 print("Game over!")
                 sys.exit()
         
-        for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
                     shot.kill()
+                    asteroid.kill()
         
         for drawings in drawable:
             drawings.draw(screen)
